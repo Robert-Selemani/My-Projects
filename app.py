ef176bde185b67d4360292a73a84bddbf62f9013
Robert Selemani <61738125+Robert-Selemani@users.noreply.github.com>
2024-06-25 18:33:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 import streamlit as st
 import os 
 
-#from langchain_community.document_loaders import WebBaseLoader
-
 from langchain_community.document_loaders import PyPDFLoader
 
 from langchain_community.vectorstores import Chroma
-
-#from langchain_community import embeddings
 
 from langchain_community.embeddings import OllamaEmbeddings
 
@@ -21,25 +17,16 @@ from langchain_core.prompts import ChatPromptTemplate
 
 from langchain.text_splitter import CharacterTextSplitter 
 
+
 #Process PDFs
-
-
- # URL processing
 def process_input(docs_list, question):
     model_local = Ollama(model="mistral")
     
-    # Convert string of URLs to list
-    #urls_list = urls.split("\n")
-    #docs = [PyPDFLoader(pdf) for pdf in pdf_docs]
-    #docs_list = [item for sublist in docs for item in sublist]
-    
     #split the text into chunks
-    
     text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=7500, chunk_overlap=100)
     doc_splits = text_splitter.split_documents(docs_list)
     
     #convert text chunks into embeddings and store in vector database
-
     vectorstore = Chroma.from_documents(
         documents=doc_splits,
         collection_name="rag-chroma",
@@ -48,11 +35,11 @@ def process_input(docs_list, question):
     retriever = vectorstore.as_retriever()
     
     #perform the RAG 
-    
     after_rag_template = """Answer the question based only on the following context:{context} Question:{question}"""
+    
     #print(after_rag_template)
-
     after_rag_prompt = ChatPromptTemplate.from_template(after_rag_template)
+    
     #print(after_rag_prompt)
     after_rag_chain = (
         {"context": retriever, "question": RunnablePassthrough()}
@@ -63,46 +50,38 @@ def process_input(docs_list, question):
 
     return after_rag_chain.invoke(question)
 
-
-    # streamlit UI
-
-st.title(" Chat with your SOPs")
-st.write("Enter SOPs and a question to query the documents")
+#---------------------------------------------------------------------------------------------------------------------------------------------------------
+# streamlit UI
+st.title("Chat with your documents")
+st.write("Enter the documents and a question to query the documents")
 
 
 #UI for input fields 
-
-#urls = st.text_area("Enter URLs separated by a New Line", height =150)
-UploadedFiles = st.file_uploader("Upload your SOPs here and click on 'Upload'", accept_multiple_files=True)
+UploadedFiles = st.file_uploader("Upload your documents here and click on 'Upload'", accept_multiple_files=True)
 
 if st.button("Upload"):
-    with st.spinner("Uploading SOPs"):
-    #get the pdf text
+    with st.spinner("Uploading documents"):
+    
+     #get the pdf text
         DocumentList = []
         for UploadedFile in UploadedFiles:
-            with open(os.path.join("UploadedSOPs",UploadedFile.name), "wb") as f:
+            with open(os.path.join("UploadedDocs",UploadedFile.name), "wb") as f:
                 f.write(UploadedFile.getbuffer())
-            DocumentList.append(os.path.join("UploadedSOPs",UploadedFile.name))
-
-        
-        
-
-        
-        
+            DocumentList.append(os.path.join("UploadedDocs",UploadedFile.name))
 
 question = st.text_input("Question")
 
 # button = st.text_input("Question")
-
 if st.button('Query Documents'):
     with st.spinner('Processing ....'):
-       #print(DocumentList)
-        DocumentList = os.listdir(r'UploadedSOPs')
-        docs = [PyPDFLoader(os.path.join("UploadedSOPs",pdf)).load() for pdf in DocumentList]
+       
+     #print(DocumentList)
+        DocumentList = os.listdir(r'UploadedDocs')
+        docs = [PyPDFLoader(os.path.join("UploadedDocs",pdf)).load() for pdf in DocumentList]
         docs_list = [item for sublist in docs for item in sublist]
 
-
         answer =process_input(docs_list, question)
+        
         #print(answer)
         st.text_area("Answer", value=answer, height=300)
 
